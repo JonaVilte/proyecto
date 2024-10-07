db4f1c14e6487bd7ce33afc696e0e7863cbb761d
Jonatan <togo36918@gmail.com>
2024-10-07 09:26:28 -0300

    Se agrego un def que me permite seleccionar un dato de la tabla dependiendo del ID

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import mariadb
 
 app = Flask(__name__)
 
-@app.route("/color")
+@app.route("/products")
 def Colores():
     mari = mariadb.connect(
         user = "uniondepo",
@@ -12,7 +12,7 @@ def Colores():
         database= "uniondepo"
     )
     cur = mari.cursor()
-    cur.execute("SELECT * FROM color")
+    cur.execute("SELECT * FROM products")
 
     color = [column[0] for column in cur.description]
     
@@ -21,3 +21,22 @@ def Colores():
         tabla.append(dict(zip(color, row)))
 
     return jsonify(tabla)
+
+
+@app.route("/products/<int:id>")
+def Tarta(id):
+    mari = mariadb.connect(
+        user = "uniondepo",
+        password ="uniondepo111",
+        host ="10.9.120.5",
+        database= "uniondepo"
+    )
+    cur = mari.cursor()
+    cur.execute("SELECT * FROM products WHERE ID= ?", (id,))
+
+    color = [column[0] for column in cur.description]
+    
+    tabla = [dict(zip(color, row)) for row in cur.fetchall()]
+
+    return jsonify(tabla)
+
